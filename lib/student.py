cd0c9675fb54576d73578217fecb8af0fe2c7ca5
Albert Cheruiyot <bettalbert99@gmail.com>
2024-05-30 13:54:10 +0300

    First commit

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,19 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # Call the __init__ method of the User class
+        self.knowledge = []
     
-    def learn(self):
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)
+        
         pass
+
+student1 = Student("Albert", "Amina")
+print(student1.first_name)  
+print(student1.last_name)  
+
+student1.learn("Python basics")
+student1.learn("Java Basics")
+print(student1.knowledge) 
